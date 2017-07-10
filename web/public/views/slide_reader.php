e726e6435142ad7ff27ba1d40245960e0193f28f
Evan Simkowitz <esimkowitz@wustl.edu>
2017-07-10 11:47:48 -0500

    cleaning up display

--- web/public/views/slide_reader.php
+++ web/public/views/slide_reader.php
@@ -15,7 +15,6 @@
   <link rel="shortcut icon" type="image/x-icon" href="https://<?php echo(urlencode($_SERVER['SERVER_NAME']));?>/favicon.ico" />
   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   <script src=" https://<?php echo(urlencode($_SERVER[ 'SERVER_NAME']));?>/vue.js"></script>
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -64,46 +63,6 @@
           });
       }
     });
-    // Find all iframes
-    var $iframe = $("#slide_frame");
-    // Find &#x26; save the aspect ratio for all iframes
-    $iframe.data("ratio", 9 / 16)
-      // Remove the hardcoded width &#x26; height attributes
-      .removeAttr("width")
-      .removeAttr("height");
-
-    function resizeIframe() {
-      //   if ($(window).width() > 3 * ($("#bookmark_list").width())) {
-      //     var width = $(window).width() - 1.4 * $("#bookmark_list").width();
-      //     if (width * $iframe.data("ratio") > 0.94 * ($(window).innerHeight() - $("#header").height())) {
-      //       width = 0.95 * (($(window).innerHeight() - $("#header").height()) / $iframe.data("ratio"));
-      //     }
-      //     // $iframe.parent().width(width).height(width * $iframe.data("ratio"));
-      //     // $iframe.parent().css("position", "fixed");
-      //     $iframe.parent().css("order", "3");
-
-      //     // $("#bookmark_list").css("padding-top", "7em");
-      //   } else {
-      //     var width = 0.98 * $(window).width();
-      //     // $iframe.parent().width(width).height(width * $iframe.data("ratio"));
-      //     // $iframe.parent().css("position", "relative");
-      //     $iframe.parent().css("order", "1");
-
-      //     // $("#bookmark_list").css("padding-top", "0");
-      //   }
-    }
-    resizeIframe();
-    var $last_header_height = 0;
-
-    // function makeMobileFriendly() {
-    //   if ($("#header").height() !== $last_header_height) {
-    //     $("#bookmark_list").css("padding-top", $("#header").height());
-    //     $last_header_height = $("#header").height();
-    //   }
-    // }
-    // makeMobileFriendly();
-    // Resize the iframes when the window is resized
-    // $(window).resize(resizeIframe).resize(makeMobileFriendly).resize();
   </script>
 </body>
 
